9113bc33ef82c99daa6b77b39b2af7a837ed803a
GSCrawley <gscrawley@gmail.com>
2020-02-13 12:08:47 -0800

    content component built

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PageHeader from './PageHeader'
+import PageContent from './PageContent'
+
+
 
 
 function App() {
   return (
     <div className="App">
       <PageHeader />
+      <PageContent />
       </div>
       );
     }
---
+++ src/PageContent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Project from './Project'
+
+
+
+function PageContent() {
+ return (
+ <div>
+ <Project title="Cutest Puppy" image="/images/cute_dog.jpg" link="#" />
+ <Project title="Ganesha" image="/images/ganesha.jpg" link="#" />
+ <Project title="Fender Telecaster" image="/images/Fender,jpg" link="#" />
+ <Project title="Gibson Les Paul" image="/images/Gibson.jpg" link="#" />
+ <Project title="Yours Truly" image="/images/shoze.jpg" link="#" />
+ <Project title="Sydney Opera House" image="/images/sydney.jpeg" link="#" />
+ </div>
+ )
+}
+
+export default PageContent
