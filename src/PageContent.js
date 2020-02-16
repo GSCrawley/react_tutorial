9d98869b62f051d78af1628c0962531f13999967
GSCrawley <gscrawley@gmail.com>
2020-02-16 13:01:36 -0800

    about to scrap Project components so saving jic

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,18 +1,18 @@
+import data from './data'
 import React from 'react'
 import Project from './Project'
 import './PageContent.css'
 
 
-
 function PageContent() {
  return (
  <div className="PageContent">
- <Project title="Cutest Puppy" image="/images/cute_dog.jpg" link="#" />
- <Project title="Ganesha" image="/images/ganesha.png" link="#" />
- <Project title="Fender Telecaster" image="/images/Fender.jpg" link="#" />
- <Project title="Gibson Les Paul" image="/images/Gibson.jpg" link="#" />
- <Project title="Yours Truly" image="/images/shoze.jpg" link="#" />
- <Project title="Sydney Opera House" image="/images/sydney.jpeg" link="#" />
+ <Project title="Cutest Puppy" image="/images/cute_dog.jpg" link="/puppies" />
+ <Project title="Ganesha" image="/images/ganesha.png" link="/hindu_gods" />
+ <Project title="Fender Telecaster" image="/images/Fender.jpg" link="/guitars" />
+ <Project title="Gibson Les Paul" image="/images/Gibson.jpg" link="/guitars" />
+ <Project title="Yours Truly" image="/images/shoze.jpg" link="/me" />
+ <Project title="Sydney Opera House" image="/images/sydney.jpeg" link="/landmarks" />
  </div>
  )
 }
--- src/Project.js
+++ src/Project.js
@@ -1,18 +1,21 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 import './Project.css'
 
 function Project({ image, title, link }) {
     return (
-    <div className="ProjectImage">
-    <div className="ProjectTitle">
-    <div className="ProjectLink">
-    <img src={image} width="300" height="auto" />
-    <h3>{title}</h3>
-    <a href={link}>Link to project</a>
-    </div>
-    </div>
-    </div>
-    )
+        <div className='project'>
+        <div className="ProjectImage">
+        <div className="ProjectTitle">
+        <div className="ProjectLink">
+        <img src={image} width="300" height="auto" />
+        <h3>{title}</h3>
+        <Link to={props.link}>Link to project</Link>
+        </div>
+        </div>
+        </div>
+        </div>
+        )
    }
 
 export default Project
