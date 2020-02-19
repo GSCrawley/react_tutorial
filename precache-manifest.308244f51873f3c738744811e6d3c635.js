96023597f14130f0f81510646112d90f550e2e71
GSCrawley <gscrawley@gmail.com>
2020-02-18 16:38:01 -0800

    Updates

---
+++ precache-manifest.308244f51873f3c738744811e6d3c635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d73396812d923a17d2c9f172e07ae3",
+    "url": "/react_tutorial/index.html"
+  },
+  {
+    "revision": "3fcad7b18b332c962eae",
+    "url": "/react_tutorial/static/css/main.8b35321e.chunk.css"
+  },
+  {
+    "revision": "0c8cf92f75cd51456ef4",
+    "url": "/react_tutorial/static/js/2.2db2dc2a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_tutorial/static/js/2.2db2dc2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fcad7b18b332c962eae",
+    "url": "/react_tutorial/static/js/main.7b528737.chunk.js"
+  },
+  {
+    "revision": "f262da74a5662c5c1fce",
+    "url": "/react_tutorial/static/js/runtime-main.ed4d340a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
